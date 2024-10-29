ba5f6ab2e8c5a10c17811410fed63cce66bced25
fenster01-ops <zotina1212@icloud.com>
2024-10-29 10:25:39 +0300

    Add files via upload

---
+++ lesson76/main.js
@@ -0,0 +1,13 @@
+const firstName = 'Egor'
+const age = 16
+const city = 'Kaluga'
+const studies = 'in Aroken.ru'
+const play = 'guitar'
+
+
+
+console.log(firstName)
+console.log(age)
+console.log(city)
+console.log(studies)
+console.log(play)
