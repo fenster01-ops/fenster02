cf0d8c90ea8fe6bd813776fe19fedbea104bfd3b
fenster01-ops <zotina1212@icloud.com>
2024-10-30 17:43:21 +0300

    Add files via upload

---
+++ lesson81/main.js
@@ -0,0 +1,16 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+
+
+for (let i = 0; i < numbers.length; i++) {
+
+    if (numbers[i] % 2 === 0) {
+        console.log(numbers[i]);
+    }
+}
+
+let color = ['Красный', 'Оранжевый', 'Желтый', 'Зеленый', 'Синий', 'Фиолетовый', 'Голубой']
+
+for (let p = color.length; p >= 0; p--) {
+    console.log(color[p])
+
+}
