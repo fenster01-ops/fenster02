07c3b483de89f2aa4a2aed757f5d5cc174045a6a
fenster01-ops <zotina1212@icloud.com>
2024-10-30 13:41:47 +0300

    Update main.js

--- lesson80/main.js
+++ lesson80/main.js
@@ -1,31 +1,31 @@
-// let i = 2
+ let i = 2
 
-// for (let i = 2; i <=12; i++) {
-//     if (i % 2 === 0) {
-//         console.log(i)
-//     }
-// }
+ for (let i = 2; i <=12; i++) {
+    if (i % 2 === 0) {
+         console.log(i)
+     }
+ }
 
-// let = 10
+ let = 10
 
-// for (let i = 10; i >=1; i--) {
-//     console.log(i)
-// }
+ for (let i = 10; i >=1; i--) {
+     console.log(i)
+ }
 
-// let i = 2
+ let i = 2
 
-// while (i <= 12) {
-//     if (i % 2 === 0) {
+ while (i <= 12) {
+     if (i % 2 === 0) {
         
-//         console.log(i)
-//         i
-//     }
-// }
+         console.log(i)
+         i++
+     }
+ }
 
-// let i = 10
+ let i = 10
 
-// while (i >=1) {
+ while (i >=1) {
 
-//     console.log(i)
-//     i--
-// }
+     console.log(i)
+     i--
+ }
