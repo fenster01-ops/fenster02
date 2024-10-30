af4b366f09253b38e997d89cb01c34c9ae37929b
fenster01-ops <zotina1212@icloud.com>
2024-10-30 13:37:55 +0300

    Add files via upload

---
+++ lesson80/main.js
@@ -0,0 +1,31 @@
+// let i = 2
+
+// for (let i = 2; i <=12; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+// let = 10
+
+// for (let i = 10; i >=1; i--) {
+//     console.log(i)
+// }
+
+// let i = 2
+
+// while (i <= 12) {
+//     if (i % 2 === 0) {
+        
+//         console.log(i)
+//         i
+//     }
+// }
+
+// let i = 10
+
+// while (i >=1) {
+
+//     console.log(i)
+//     i--
+// }
