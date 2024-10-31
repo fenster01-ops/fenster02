34c6a9d425dcffd266274b7b5cc08fbb1d273356
fenster01-ops <zotina1212@icloud.com>
2024-10-31 12:46:01 +0300

    Add files via upload

---
+++ lesson82/main.js
@@ -0,0 +1,21 @@
+function welcome(Name) {
+    return 'Hello  ' + 'Александр'
+}
+
+let GOGH = welcome('Александр')
+console.log(GOGH)
+
+const number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+function greet(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > 10) {
+            console.log(numbers[i]);
+            
+        }
+    }
+}
+greet(number)
+
+// 3 задание не сделал не понимаю как его делать
+
